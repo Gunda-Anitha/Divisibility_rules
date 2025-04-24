edd9d5e22c4f08458cc7799ac99c5765413c8968
Gunda-Anitha <gundaanitha5@gmail.com>
2025-04-24 16:39:22 +0530

    Add files via upload

---
+++ divisibility rules/divisible_by_2/divisible by 2.py
@@ -0,0 +1,8 @@
+def divisible_by_2():
+    num=int(input("Enter num : "))
+    if num%2==0:
+        print(f"{num} is divisible by 2")
+    else:
+        print(f"{num} is not divisible by 2")
+if __name__ == "__main__":
+    divisible_by_2()
